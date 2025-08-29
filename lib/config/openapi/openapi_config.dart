903601c06b2657429500466a3eb1ffbc9101edc4
João Sereia <joao.lo.sereia@gmail.com>
2025-08-29 07:57:36 -0300

    feat(config/openapi): add OpenAPI config

---
+++ lib/config/openapi/openapi_config.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: avoid_dynamic_calls
+
+import 'dart:convert';
+
+import 'package:quadroflow/src/domain/entities/app_settings.dart';
+import 'package:vaden/vaden.dart';
+import 'package:vaden/vaden_openapi.dart';
+
+@Configuration()
+final class OpenAPIConfig {
+  const OpenAPIConfig();
+
+  @Bean()
+  OpenApi openApi(OpenApiConfig config, AppSettings settings) {
+    return OpenApi(
+      version: '3.0.0',
+      tags: config.tags,
+      paths: config.paths,
+      servers: [config.localServer],
+      components: Components(schemas: config.schemas),
+      info: Info(
+        title: settings.openApi.title,
+        version: settings.openApi.version,
+        description: settings.openApi.description,
+      ),
+    );
+  }
+
+  @Bean()
+  SwaggerUI swaggerUI(OpenApi openApi, AppSettings settings) {
+    return SwaggerUI(
+      jsonEncode(openApi.toJson()),
+      deepLink: true,
+      title: settings.openApi.title,
+    );
+  }
+}
---
+++ lib/config/openapi/openapi_controller.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: avoid_dynamic_calls
+
+import 'dart:async';
+
+import 'package:quadroflow/src/domain/entities/app_env.dart';
+import 'package:vaden/vaden.dart';
+import 'package:vaden/vaden_openapi.dart' hide Response;
+
+@Controller('/docs')
+class OpenAPIController {
+  const OpenAPIController(this._swaggerUI, this._environment);
+
+  final SwaggerUI _swaggerUI;
+  final AppEnvironment _environment;
+
+  @Get('/')
+  FutureOr<Response> getSwaggerUI(Request request) {
+    final isEnabled = _environment.openApi.enabled;
+    if (isEnabled == true) return _swaggerUI.call(request);
+
+    return Response.notFound('Not Found');
+  }
+
+  @Get('/openapi.json')
+  Response getOpenApiJSON(Request request) {
+    final isEnabled = _environment.openApi.enabled;
+    if (isEnabled == true) {
+      return Response.ok(
+        _swaggerUI.schemaText,
+        headers: {'Content-Type': 'application/json'},
+      );
+    }
+
+    return Response.notFound('Not Found');
+  }
+}
